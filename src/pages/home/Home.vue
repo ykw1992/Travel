d406f0f346360429020a7d02ea35b2f7d5a7077b
itaffy <itaffy@qq.com>
2019-03-01 16:55:29 +0800

    project init

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <div>Hello World</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
+
+<style>
+  div{
+    font-size:30px;
+  }
+</style>
