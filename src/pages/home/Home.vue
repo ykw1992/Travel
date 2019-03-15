da3705fd6cc0594c3d60374fe94cb7914ecd6331
itaffy <itaffy@qq.com>
2019-03-15 11:54:14 +0800

    add recommend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
     <home-swiper></home-swiper>
     <home-icon></home-icon>
     <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 <script>
@@ -11,13 +12,15 @@ import HomeHeader from './components/header'
 import HomeSwiper from './components/swiper'
 import HomeIcon from './components/icons'
 import HomeRecommend from './components/recommend'
+import HomeWeekend from './components/weekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcon,
-    HomeRecommend
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
--- src/pages/home/components/weekend.vue
+++ src/pages/home/components/weekend.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div class="title">热销推荐</div>
+    <div class="title">周末去哪</div>
       <ul>
         <li class="item" v-for="item of recommendList" :key='item.id'>
+          <div class="item-img-wrapper">
             <img :src="item.imgUrl"  alt="" class="item-img">
+          </div>
             <div class="item-info border-bottom">
               <p class="item-title">{{item.title}}</p>
               <p class="item-desc">{{item.desc}}</p>
-              <button class="item-button">查看详情</button>
             </div>
         </li>
       </ul>
@@ -15,22 +16,22 @@
 </template>
 <script>
 export default {
-  name: 'HomeRecommend',
+  name: 'HomeWeekend',
   data () {
     return {
       recommendList: [{
         id: '0001',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
         title: '上海迪士尼乐园',
         desc: '上海迪士尼乐园'
       }, {
         id: '0002',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
         title: '上海迪士尼乐园',
         desc: '上海迪士尼乐园'
       }, {
         id: '0003',
-        imgUrl: 'http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_200x200_2039e660.jpg',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/7d/df6ff468331c43.jpg_r_640x214_1f3783d0.jpg',
         title: '上海迪士尼乐园',
         desc: '上海迪士尼乐园'
       }]
@@ -46,28 +47,20 @@ export default {
     line-height .8rem
     background #eee
     text-indent .2rem
-  .item
-    height 2.1rem
-    display flex
+  .item-img-wrapper
+    height 0
     overflow hidden
+    padding-bottom  37.1%
     .item-img
-      width 1.7rem
-      height 1.9rem
-      padding .1rem
-    .item-info
-      flex 1
-      padding .1rem
-      min-width 0
-      .item-title
-        line-height .54rem
-        font-size .32rem
-        ellipsis()
-      .item-desc
-        line-height .4rem
-        color #ccc
-        ellipsis()
-      .item-button
-        background #ff9300
-        border-radius .06rem
-        padding 0 .2rem
+      width 100%
+  .item-info
+    padding .1rem
+    .item-title
+      line-height .54rem
+      font-size .32rem
+      ellipsis()
+    .item-desc
+      line-height .4rem
+      color #ccc
+      ellipsis()
 </style>
